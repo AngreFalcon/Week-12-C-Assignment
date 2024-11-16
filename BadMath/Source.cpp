5a3f20da14591a822334b9cdb0f8299d177370c1
Sorre <angrepenguin@gmail.com>
2024-11-15 22:29:09 -0500

    added input validation for integer prompts fixed an order of operations bug in the average() fixed a variable assignment bug in main()

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <iomanip>
-using namespace std;
+#include "commonutils.hpp"
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
+float average(int i1, int i2, int i3) {
+	return static_cast<float>(i1 + i2 + i3) / 3.0f;
 }
 
-int main()
-{
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+int main() {
+	int n1 = 0, n2 = 0, n3 = 0;
+	std::cout << "This program calculates the average of three numbers.\n";
+	n1 = getNum<int>("First number: ");
+	n2 = getNum<int>("Second number: ");
+	n3 = getNum<int>("Third number: ");
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	std::cout << std::setprecision(1) << std::fixed << "The average is " << a << std::endl;
 
 	return 0;
 }
